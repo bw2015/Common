0ceeadf8a8f6742e3a88d42d1a144581d365e7ac
Peter <peter@google.com>
2021-12-07 14:17:29 +0800

    工具库

--- kangle/config.js
+++ kangle/config.js
@@ -18,21 +18,15 @@
     // 请求远程IP地址
     !function () {
         var myHeaders = new Headers();
-        myHeaders.append("Connection", "keep-alive");
-        myHeaders.append("Pragma", "no-cache");
         myHeaders.append("Cache-Control", "no-cache");
-        myHeaders.append("sec-ch-ua", "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"");
         myHeaders.append("Content-Type", "application/json");
         myHeaders.append("sec-ch-ua-mobile", "?0");
         myHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
-        myHeaders.append("sec-ch-ua-platform", "\"Windows\"");
         myHeaders.append("Accept", "*/*");
-        myHeaders.append("Origin", "chrome-extension://coohjcphdfgbiolnekdpbcijmhambjff");
         myHeaders.append("Sec-Fetch-Site", "none");
         myHeaders.append("Sec-Fetch-Mode", "cors");
         myHeaders.append("Sec-Fetch-Dest", "empty");
         myHeaders.append("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,th;q=0.6");
-        myHeaders.append("Accept-Encoding", "deflate, gzip");
         var raw = JSON.stringify(data);
 
         var requestOptions = {
